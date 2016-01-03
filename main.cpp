d733be7cc09e903048fe0d034e9ba351124e7ef3
qnnnnez <qnnnnez@live.com>
2016-01-03 18:23:01 +0800

    implement command line interface

--- main.cpp
+++ main.cpp
@@ -1,18 +1,47 @@
 #include "pakfile.h"
 #include "process.h"
+#include "native.h"
 #include <sstream>
 #include <fstream>
 
 using namespace std;
 using namespace scpak;
 
-int main(int argc, char *argv[])
+void printUsage(int argc, char *argv[])
 {
-	ifstream fin("Content.pak", ios::binary);
-	PakFile pak;
-	pak.load(fin);
-	fin.close();
-	unpack(pak, "./Content");
-    return 0;
+	string programPath = argv[0];
+	size_t i = programPath.rfind(pathsep);
+	string programName = programPath.substr(i);
+	cout << "Usage: " << programName << " <directory> | <pakfile>" << endl;
 }
 
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		printUsage(argc, argv);
+		return 1;
+	}
+	string path = argv[1];
+	if (isDirectory(path.c_str()))
+	{
+		PakFile pak = pack(path);
+		ofstream fout(path + ".pak", ios::binary);
+		pak.save(fout);
+		fout.close();
+	}
+	else if (isNormalFile(path.c_str()))
+	{
+		size_t i = path.rfind(".pak");
+		string directoryName;
+		if (i == string::npos)
+			directoryName = path + "_unpack";
+		else
+			directoryName = path.substr(0, i);
+		ifstream fin(path, ios::binary);
+		PakFile pak;
+		pak.load(fin);
+		unpack(pak, directoryName);
+	}
+    return 0;
+}
