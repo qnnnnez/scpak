e2ebeffa3adea90e0baccc5af1eb3a51ba35c462
qnnnnez <qnnnnez@live.com>
2017-01-25 16:00:09 +0800

    add tinyxml2 as a submodule

--- stb.cpp
+++ stb.cpp
@@ -1,9 +1,6 @@
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb/stb_image.h"
 
-#if defined(_MSC_VER)
-# pragma warning(disable: 4996)
-#endif
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb/stb_image_write.h"
 
